ef7fba65ca7794209dc371ac57a33e98d280e85c
Your Name <you@example.com>
2021-09-21 14:51:05 +0530

    MainActivity added

---
+++ app/src/main/java/co/in/nextgencoder/toddlersbook/ClassOneActivity.java
@@ -0,0 +1,14 @@
+package co.in.nextgencoder.toddlersbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClassOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_one);
+    }
+}
