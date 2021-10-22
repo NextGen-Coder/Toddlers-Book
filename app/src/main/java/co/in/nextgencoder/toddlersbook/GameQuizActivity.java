28108d9d28986127922ec28ca8d6887e24f3f86b
manali <manaliwirmalwar@gmail.com>
2021-10-22 13:16:44 +0530

    Game Quiz Activity Design

---
+++ app/src/main/java/co/in/nextgencoder/toddlersbook/GameQuizActivity.java
@@ -0,0 +1,28 @@
+package co.in.nextgencoder.toddlersbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class GameQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_quiz);
+    }
+
+
+    public void option0Clicked(View view) {
+    }
+
+    public void option1Clicked(View view) {
+    }
+
+    public void option2Clicked(View view) {
+    }
+
+    public void option3Clicked(View view) {
+    }
+}
