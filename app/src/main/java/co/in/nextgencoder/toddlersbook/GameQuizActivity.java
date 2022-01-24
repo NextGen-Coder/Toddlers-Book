e180d22077b9b272cee541faa81dfd2c39df5c7c
AishwaryaBawankule <aishwaryabawankule05@gmail.com>
2022-01-24 12:49:50 +0530

    FlipCardGame Added

--- app/src/main/java/co/in/nextgencoder/toddlersbook/GameQuizActivity.java
+++ app/src/main/java/co/in/nextgencoder/toddlersbook/GameQuizActivity.java
@@ -10,24 +10,30 @@ import android.widget.ImageView;
 public class GameQuizActivity extends AppCompatActivity {
 
     Dialog myDialog;
-    ImageView imageView;
+    ImageView imageViewZero;
+    ImageView imageViewOne;
+    ImageView imageViewTwo;
+    ImageView imageViewThree;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_quiz);
         myDialog = new Dialog(this);
+        imageViewZero = findViewById(R.id.option0);
+        imageViewOne = findViewById(R.id.option1);
+        imageViewTwo = findViewById(R.id.option2);
+        imageViewThree = findViewById(R.id.option3);
     }
 
 //    public void correctOption(View view){
-//        if()
+//        if(imageViewZero.equals())
 //    }
     public void option0Clicked(View view) {
 
         myDialog.setContentView(R.layout.correct);
         myDialog.show();
-
-
 
     }
 
